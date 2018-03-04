ddea5ee97c84f201500036aac5ebb1146d731780
Mark Kreitler <mkreitler@hotmail.com>
2018-03-03 20:37:04 -0700

    First commit.

--- IntegratedBasketball/ViewController.swift
+++ IntegratedBasketball/ViewController.swift
@@ -1,25 +1,34 @@
 //
 //  ViewController.swift
-//  IntegratedBasketball
 //
-//  Created by Mark Kreitler on 3/3/18.
-//  Copyright © 2018 Mark Kreitler. All rights reserved.
+//  Created by Adam Venturella on 10/28/15.
+//  Updated by Martin Straub on 15/03/2017.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var unityView: UIView?
+    
+    @IBAction func startUnity(sender: AnyObject) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.startUnity()
+        
+        unityView = UnityGetGLView()!
+        
+        self.view!.addSubview(unityView!)
+        unityView!.translatesAutoresizingMaskIntoConstraints = false
+        
+        // look, non-full screen unity content!
+        let views = ["view": unityView]
+        let w = NSLayoutConstraint.constraints(withVisualFormat: "|-20-[view]-20-|", options: [], metrics: nil, views: views)
+        let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-75-[view]-50-|", options: [], metrics: nil, views: views)
+        view.addConstraints(w + h)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func stopUnity(sender: AnyObject) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.stopUnity()
+        unityView!.removeFromSuperview()
     }
-
-
 }
-
